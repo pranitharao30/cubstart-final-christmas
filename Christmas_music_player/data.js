219091f10a8287c27c53cddcd3426f859e017e61
haifulun <94204208+haifulun@users.noreply.github.com>
2022-12-06 03:31:04 +0800

    Add files via upload

---
+++ Christmas_music_player/data.js
@@ -0,0 +1,32 @@
+let songs =[
+  {
+    name: 'Santa Tell Me',
+    path: 'Songs/Song2.mp3',
+    artist: 'Ariana Grande',
+    cover: 'Images/image2.png'
+  },
+  {
+    name: 'Jingle Bell Rock',
+    path: 'Songs/Song1.mp3',
+    artist: 'Glee',
+    cover: 'Images/image1.png'
+  },
+  {
+    name: 'Feliz Navidad',
+    path: 'Songs/Song3.mp3',
+    artist:'Boney M.',
+    cover:'Images/image3.jpeg'
+  },
+  {
+    name: 'We Wish You A Merry Christmas',
+    path: 'Songs/Song4.mp3',
+    artist:'Melissa Lyons ft.Shauntia Flemin',
+    cover:'Images/image4.jpeg'
+  },
+  {
+    name: 'It’s beginning to look a lot like Christmas',
+    path: 'Songs/Song5.mp3',
+    artist:'Michael Bublé',
+    cover:'Images/image5.jpeg'
+  }
+]
